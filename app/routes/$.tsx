e67e12e2e438500d2805dd0a0b5e3e236db426fd
Gracias <31932887+graciasc@users.noreply.github.com>
2024-05-11 22:31:22 -0600

    Update $.tsx

--- app/routes/$.tsx
+++ app/routes/$.tsx
@@ -1,4 +1,5 @@
 // splat route --> this is a catch all unknown routes
+// make a fun custom 404 page (using Adobe Firefly)
 export default function FourOhFour() {
   return <div> Custom 404 Page </div>;
 }
