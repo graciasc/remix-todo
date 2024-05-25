7c0f981a754fe8c1bef714ebc987f0d4d3294f18
Gracias Claude <gracias.claude@outlook.com>
2024-05-24 22:42:07 -0600

    shadcn/ui / dependencies / config
    - played around with a button from shadcn/ui
    - add new dependencies such as:
      - `tailwind-animate`
      - `cn`
      - `tailwind-merge`
    - add path in tsconfig to handle mapp `#app/*`
    - add `lib/util.tsx` file as discussed in the shadcn docs and the cva docs

--- app/routes/public+/morty_+/index.tsx
+++ app/routes/public+/morty_+/index.tsx
@@ -1,29 +1,33 @@
 import * as Form from "@radix-ui/react-form";
 
-import { Input } from "../../../components/input";
+import { Button } from "#app/components/button";
+import { Input } from "#app/components/input";
 export default function MortyIndex() {
   return (
-    <Input className="p-12" variant={"default"}>
-      <Form.Field name="search" className="flex space-x-5">
-        <div className="flex items-center">
-          <Form.Label className="space-x-2 text-xl bold">Search</Form.Label>
-        </div>
-        <Form.Message match="valueMissing" className="border-amber-50 p-2">
-          This hit the rick and morty api
-        </Form.Message>
-        <Form.Message match="typeMismatch">
-          Please provide some text
-        </Form.Message>
-        <Form.Control asChild>
-          <input
-            className="placeholder:italic placeholder:text-slate-400  w-1/4 block bg-white w-full border border-slate-300 rounded-md py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-rose-500 focus:ring-rose-500 focus:ring-1 sm:text-sm"
-            placeholder="Search for anything..."
-            type="text"
-            name="search"
-            required
-          />
-        </Form.Control>
-      </Form.Field>
-    </Input>
+    <div>
+      <Input className="p-12" variant={"default"}>
+        <Form.Field name="search" className="flex space-x-5">
+          <div className="flex items-center">
+            <Form.Label className="space-x-2 text-xl bold">Search</Form.Label>
+          </div>
+          <Form.Message match="valueMissing" className="border-amber-50 p-2">
+            This hit the rick and morty api
+          </Form.Message>
+          <Form.Message match="typeMismatch">
+            Please provide some text
+          </Form.Message>
+          <Form.Control asChild>
+            <input
+              className="placeholder:italic placeholder:text-slate-400  w-1/4 block bg-white w-full border border-slate-300 rounded-md py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-rose-500 focus:ring-rose-500 focus:ring-1 sm:text-sm"
+              placeholder="Search for anything..."
+              type="text"
+              name="search"
+              required
+            />
+          </Form.Control>
+        </Form.Field>
+      </Input>
+      <Button className="m-4">Testing Button Component</Button>
+    </div>
   );
 }
